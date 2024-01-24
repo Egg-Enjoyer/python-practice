f1516dd6a39b9b40646f563624cd43d933bb84bc
jrhw <30939167+Egg-Enjoyer@users.noreply.github.com>
2024-01-24 12:18:51 -0500

    Create replace_with_alphabet_position.py

---
+++ replace_with_alphabet_position.py
@@ -0,0 +1,20 @@
+import string
+
+def creat_alphabet_dic():
+    result = {}
+    alphabet = list(string.ascii_lowercase)
+    position = 1
+    for letter in alphabet:
+        result[letter] = position
+        position += 1
+    return result
+
+
+def alphabet_position(text):
+    text = text.lower()
+    result = ""
+    position_dic = creat_alphabet_dic()
+    for letter in text:
+        if letter in list(string.ascii_lowercase):
+            result += f"{position_dic[letter]} "
+    return result[:-1]
